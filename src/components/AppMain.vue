99e0306fdf7562a15a9ef52b15acc617a35af9f1
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-30 16:26:14 +0200

    milestone 5

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,9 @@
 <template>
     <div class="container">
+
         <div class="row">
             <div class="col-3 g-3" v-for="project in projects">
-                <div class="card">
-                    <img v-if="project.image" :src="`${this.baseUrl}/storage/app/${project.image}`" class="card-img-top"
-                        alt="...">
-                    <img v-else
-                        src="https://thumbs.dreamstime.com/b/nessuna-foto-disponibile-o-immagine-mancante-39680127.jpg"
-                        alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ project.title }}</h5>
-                        <p class="card-text">{{ project.description }}</p>
-                        <a href="#" class="btn btn-primary">Da implementare</a>
-                    </div>
-                </div>
+                <AppProjectCard :projectProp="project" :baseUrlProp="this.baseUrl"></AppProjectCard>
             </div>
         </div>
     </div>
@@ -21,14 +11,17 @@
 
 <script>
 import axios from 'axios';
+import AppProjectCard from './AppProjectCard.vue';
 
 export default {
     name: "AppMain",
+    components: {
+        AppProjectCard
+    },
     data() {
         return {
             projects: [],
             baseUrl: 'http://localhost:8000'
-
         }
     },
     methods: {
---
+++ src/components/AppProjectCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <img v-if="this.projectProp.image" :src="`${this.baseUrlProp}/storage/${this.projectProp.image}`"
+            class="card-img-top" alt="...">
+        <img v-else src="https://thumbs.dreamstime.com/b/nessuna-foto-disponibile-o-immagine-mancante-39680127.jpg" alt="">
+        <div class="card-body">
+            <h5 class="card-title">{{ this.projectProp.title }}</h5>
+            <p class="card-text">{{ this.projectProp.description }}</p>
+            <a href="#" class="btn btn-primary">Da implementare</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppProjectCard",
+    props: { projectProp: Object, baseUrlProp: String }
+}
+</script>
+
+<style></style>
