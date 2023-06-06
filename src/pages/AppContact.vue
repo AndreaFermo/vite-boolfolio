775e94fa0c1382161a098f860e2e9cd033f42dba
Andrea Fermo <andreafermo.sd@gmail.com>
2023-06-06 15:28:23 +0200

    esercizio concluso

--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -1,10 +1,19 @@
 <template>
-    <h1>QUI VA LA CONTACT</h1>
+    <div class="container">
+        <h1>QUI VA LA CONTACT</h1>
+        <ContactForm></ContactForm>
+    </div>
 </template>
 
 <script>
+import ContactForm from '../components/ContactForm.vue';
+
 export default {
-    name: "AppContact"
+    name: "AppContact",
+    components: {
+        ContactForm
+    }
+
 }
 </script>
 
